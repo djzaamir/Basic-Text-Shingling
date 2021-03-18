4837ae677c23addd5f049fb4646b3d0265e3eccd
Aamir <djzaamir@gmail.com>
2021-03-18 19:17:35 +0500

    Added code for generating sparse Matrix for two sets of shingles (Two Documents)

--- src/main/java/TextMatcher.java
+++ src/main/java/TextMatcher.java
@@ -6,17 +6,31 @@ public class TextMatcher {
 
     public static float match(String a, String b){
 
-      // Transform string to lowercase
+      // Text normalization
       String aNormalized = normalizeText(a);
       String bNormalized = normalizeText(b);
 
       //Generate shingles from both strings
-      ArrayList<String> shingles = generateShingles(aNormalized);
-      //To generate shingles from other set, we would be passing previous shingle set in
-     // Because we want to combine both sets for further computation
-      generateShingles(bNormalized, shingles);
+      ArrayList<String> shingles_a = generateShingles(aNormalized);
+      ArrayList<String> shingles_b = generateShingles(bNormalized);
+
+      //Merged Shingles
+      ArrayList<String> mergedShingles =  new ArrayList<>();
+      mergedShingles.addAll(shingles_a);
+      mergedShingles.addAll(shingles_b);
 
      //Generate Main Matrix (The larger original version)
+     /*
+     *              Document-A  Document-B
+     * Shingle-a          0           1
+     * Shingle-b          1           0
+     * Shingle-c          1           1
+     * Shingle-d          1           1
+     *
+     * */
+     int matrixColumnSize = 3; //Depends on the number of the documents + the first column for the actual shingle itself
+     ArrayList<ArrayList<String>> sparseMatrix = sparseMatrixGenerator(shingles_a,
+                                                                        shingles_b, mergedShingles, matrixColumnSize);
 
       //Generate random permutations, we would be generating N Permutations
 
@@ -28,12 +42,61 @@ public class TextMatcher {
       return 0.5f;
     }
 
-    private static ArrayList<String> generateShingles(String str, ArrayList<String> shingles) {
+    /*
+    * TODO
+    *  This method could probably be written in a more efficient manner
+    *  Right now it has a lot of repetitive for-loops
+    * */
+    private static ArrayList<ArrayList<String>> sparseMatrixGenerator(ArrayList<String> shingles_a, ArrayList<String> shingles_b, ArrayList<String> mergedShingles, int matrixColumnSize) {
+        ArrayList<ArrayList<String>> sparseMatrix= new ArrayList<>();
+
+        //Visit all shingles, and for each shingle make a entry in the sparseMatrix for Document-A and Document-B
+        //This entry will indicate the status of the particular shingle in the document-a and document-b.
+        for (int i = 0; i < mergedShingles.size(); i++){
+
+            String currentShingle =  mergedShingles.get(i);
+
+            ArrayList<String> entry_for_this_shingle =  new ArrayList<>(matrixColumnSize);
+
+            //Initially consider both document entries as zero
+            entry_for_this_shingle.set(0, currentShingle);
+            entry_for_this_shingle.set(1, "0");  //for document-a
+            entry_for_this_shingle.set(2, "0"); // for document-b
+
+
+
+            //First probe the existence of current shingle in Document-A set, then in Document-B set
+            for (String shingle_a:
+                 shingles_a) {
+                if (currentShingle.equals(shingle_a)){
+                    entry_for_this_shingle.set(1, "1");
+                    break;
+                }
+            }
+
+            //Now for second document
+            for (String shingle_b:
+                    shingles_b) {
+                if (currentShingle.equals(shingle_b)){
+                    entry_for_this_shingle.set(2, "1");
+                    break;
+                }
+            }
+
+            //Put current row-entry inside the main ArrayList
+            sparseMatrix.add(entry_for_this_shingle);
+        }
+
+        return sparseMatrix;
+    }
+
+    private static ArrayList<String> generateShingles(String str) {
         int start_offset = 0;
         int end_offset = start_offset + TextMatcher.shingleSize;
 
         String[] words = str.split(" ");
 
+        ArrayList<String> shingles = new ArrayList<>();
         //Extract shingles
         while (end_offset <= words.length){
             String shingle = extractNWords(words, start_offset, end_offset);
@@ -46,13 +109,6 @@ public class TextMatcher {
         return shingles;
     }
 
-    private static ArrayList<String> generateShingles(String str) {
-         //Create a new required Shingle ArrayList and pass it into overloaded generateShingle method
-        // Doing so because this because both methods are doing exactly the same thing
-       //  The only difference, one is creating the ArrayList internally and other accepting as a parameter
-      //   So simply it, i've delegated the ArrayList creation process to this overload, and remaining process to other
-       return generateShingles(str, new ArrayList<String>());
-    }
 
     private static String extractNWords(String[] words, int start_offset, int end_offset) {
         StringBuilder wordsBuilder = new StringBuilder();
